d947d957b2c203b5e1c13234b19a2989ae89ff45
Lex <lexyzh@gmail.com>
2023-01-09 03:28:51 +0100

    [update] Add em-insight-web folder
    The em-insight-web folder contains the main page, js, css and font.

---
+++ js/em.js
@@ -0,0 +1,21 @@
+function changePageStyle(colorVar){
+
+    var objCss = document.getElementById("extCss");
+    var objHd = document.getElementById("imgHd");
+    var objFt = document.getElementById("imgFt");
+
+    if(colorVar == 1){ // blue
+        objCss.setAttribute("href","./css/emsg_blue.css");
+        objHd.setAttribute("src","./image/logoft_color_navy_blue.png");
+        objFt.setAttribute("src","./image/logo_color_rev_blue.png");
+
+    }else if (colorVar == 2){ // white
+        objCss.setAttribute("href","./css/emsg_white.css");
+        objHd.setAttribute("src","./image/logoft.png");
+        objFt.setAttribute("src","./image/logo.png");
+    }else{ //green
+        objCss.setAttribute("href","./css/emsg_green.css");
+        objHd.setAttribute("src","./image/logoft_color_green.png");
+        objFt.setAttribute("src","./image/logo_color_green.png");
+    }
+}
